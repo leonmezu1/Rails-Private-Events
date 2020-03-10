b2d2122e43666d5c6109c78a9a3b55bb4ab679e6
conejitomaya <eduardobonillac@gmail.com>
2020-03-10 11:57:46 -0600

    user_id column added to event model

---
+++ db/migrate/20200310174854_add_creatorid_attendeeid_to_event.rb
@@ -0,0 +1,6 @@
+class AddCreatoridAttendeeidToEvent < ActiveRecord::Migration[5.2]
+  def change
+    add_column :events, :user_id, :integer
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,37 +1,36 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition
+# incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more
-# migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control
-# system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_310_172_835) do
-  create_table 'events', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
-    t.text 'description'
-    t.string 'category'
-    t.string 'location'
-    t.date 'event_date'
-    t.string 'event_picture'
+ActiveRecord::Schema.define(version: 2020_03_10_174854) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.string "category"
+    t.string "location"
+    t.date "event_date"
+    t.string "event_picture"
+    t.integer "user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'username'
-    t.string 'email'
-    t.string 'password_digest'
-    t.integer 'event_id'
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "event_id"
   end
+
 end
