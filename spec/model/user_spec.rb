27f73bb29552245f702cbe97fbf4bbc081d65975
conejitomaya <eduardobonillac@gmail.com>
2020-03-17 15:35:20 -0600

    rspec-rails env added to spec_helper

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,9 +1,10 @@
 RSpec.describe User, type: :model do
-  subject { User.new(name: "testname", email:"testing@usagi.com", password: "123456", password_confirmation: "123456")}
+  subject { User.new(name: "testname", email: "testing@usagi.com", password: "123456", password_confirmation: "123456")}
   before { subject.save }
 
   it 'Name should exist' do
     subject.name = nil
     expect(subject).to_not be_valid
   end
+
 end
